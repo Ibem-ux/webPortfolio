e5fdd202c39cb54a20a00e25b3211616e16bd076
Ibem <cnhovem@gmail.com>
2025-10-14 13:15:43 +0800

    jsClean

--- ibemPortfolio/jsPage.js
+++ ibemPortfolio/jsPage.js
@@ -98,3 +98,23 @@ const toggleBtn = document.getElementById("toggleFormBtn");
     postList.innerHTML = "";
     loadPosts();
   }
+
+  function openTab(evt, tabName) {
+      let i, tabcontent, tablinks;
+
+      // Hide all tab contents
+      tabcontent = document.getElementsByClassName("tab-content");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+
+      // Remove "active" class from all tabs
+      tablinks = document.getElementsByClassName("tab-link");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+      }
+
+      // Show selected tab
+      document.getElementById(tabName).style.display = "block";
+      evt.currentTarget.classList.add("active");
+    }
